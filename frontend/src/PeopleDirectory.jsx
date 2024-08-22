22ad8136bd311a99a0eb415ef2f22acefd21a6fe
UsaidRashid <rashidusaid2@gmail.com>
2024-08-22 19:02:26 +0530

    Achieved Delete Popup

--- frontend/src/PeopleDirectory.jsx
+++ frontend/src/PeopleDirectory.jsx
@@ -10,7 +10,7 @@ import {
 import Sidebar from "./Sidebar";
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faPlus, faTimes } from "@fortawesome/free-solid-svg-icons";
 import AddUser from "./AddUser";
 import UserDetailsSidebar from "./UserDetailsSidebar";
 import EditUser from "./EditUser";
@@ -263,22 +263,22 @@ export default function PeopleDirectory() {
 
   const DeleteConfirmationPopup = ({ onClose, onDelete }) => (
     <div className="fixed inset-0 bg-gray-500 bg-opacity-75 flex items-center justify-center z-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg w-80">
+      <div className="bg-white p-6 rounded-lg shadow-lg w-2/5 relative">
+        <button
+          onClick={onClose}
+          className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+        >
+          <FontAwesomeIcon icon={faTimes} />
+        </button>
         <h3 className="text-lg font-semibold mb-4">Delete Member Details</h3>
         <p className="mb-4">
           Are you sure you want to delete this member details? This action
           cannot be undone.
         </p>
-        <div className="flex justify-end space-x-4">
-          <button
-            onClick={onClose}
-            className="bg-gray-300 text-gray-800 px-4 py-2 rounded-lg hover:bg-gray-400"
-          >
-            Cancel
-          </button>
+        <div className="flex justify-end">
           <button
             onClick={onDelete}
-            className="bg-purple-500 text-white px-4 py-2 rounded-lg hover:bg-purple-600"
+            className="bg-purple-700 text-white px-4 py-2 rounded-lg hover:bg-purple-600"
           >
             Delete
           </button>
